c104d49b6950c7f3f0d702f030280862a6fc465b
DevKZ <ganetfuture@gmail.com>
2023-07-15 18:45:27 +0900

    サンプル追加

---
+++ XAML/TAB/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+
+namespace WpfApp1
+{
+
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            Uri uri = new Uri("/Page1.xaml", UriKind.Relative);
+            frame.Source = uri;
+
+
+        }
+    }
+}
---
+++ XAML/TAB/WpfApp1/WpfApp1/Page1.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Page1.xaml の相互作用ロジック
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+
+
+            Tabs = new ObservableCollection<TabItem>() {
+                new TabItem {
+                    Header = "Header1",
+                    Content = new ObservableCollection<abc>()
+                    {
+                        new abc{ text = "content1" },
+                        new abc { text = "content2" },
+                    }
+                },
+                new TabItem
+                {
+                    Header = "Header2",
+                    Content = new ObservableCollection<abc>()
+                    {
+                        new abc{ text = "content3" },
+                        new abc{ text = "content4" },
+                    }
+                }
+            };
+            tabcontrol.ItemsSource = Tabs;
+        }
+
+        public ObservableCollection<TabItem> Tabs { get; set; }
+        public class TabItem
+        {
+            public string Header { get; set; }
+            public ObservableCollection<abc> Content { get; set; }
+        }
+        public class abc
+        {
+            public string text { get; set; }
+        }
+    }
+
+}
